189860c6616a2c010b228a13b9b4a2f163aee968
Sumit Bhattacharya <er.sumitbhattacharya@gmail.com>
2017-10-27 07:36:15 -0400

    Github Upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path') ;
+
+//init App
+const app = express();
+
+//load view engine
+app.set('views', path.join(__dirname,'views')) ;
+app.set('view engine', 'pug') ;
+
+//home route
+app.get('/', function(req,res){
+    //  res.send('Hello World');
+      //res.render('index') ;
+      res.render('index',{title: 'Articles'});
+  });
+
+app.listen(3200, function(){
+  console.log('Server strted on port 3200 ...');
+
+});
