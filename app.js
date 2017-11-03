7477f877d8169d0b1a7e2389b73d627ce1d92179
Sumit Bhattacharya <er.sumitbhattacharya@gmail.com>
2017-11-02 23:53:18 -0400

    pug templates added

--- app.js
+++ app.js
@@ -10,7 +10,29 @@ app.set('view engine', 'pug') ;
 
 //home route
 app.get('/', function(req,res){
-          res.render('index',{title: 'Articles'
+    let articles = [
+      {
+        id:1,
+        title:'Article One',
+        author:'Auth 1',
+        body: 'This is art 1'
+      },
+      {
+        id:2,
+        title:'Article Two',
+        author:'Auth 2',
+        body: 'This is art 2'
+      },
+      {
+        id:3,
+        title:'Article Three',
+        author:'Auth 3',
+        body: 'This is art 3'
+      }
+    ];
+          res.render('index',{
+            title: 'Articles',
+            articles: articles
     });
   });
 
